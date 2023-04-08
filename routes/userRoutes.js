66f8237a2fb54f3a9565c02a45a5bfdceb2bbaf2
Zaidi <zaidiahsan.r@gmail.com>
2023-04-08 13:36:16 -0500

    new

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,7 +6,7 @@ const usersController = require('../controllers/usersController')
 router.route('/')
     .get(usersController.getAllUsers)
     .post(usersController.createNewUser)
-    .patch(usersController.updateUser)
+    .put(usersController.updateUser)
     .delete(usersController.deleteUser)
 
 module.exports = router
